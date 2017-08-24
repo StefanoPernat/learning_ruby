7c8150915e4613ccf9485517a7e5a69fe34d4d7b
Stefano Pernat <stefano.pernat@gmail.com>
2017-08-24 19:15:38 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # here I have to define roots
+  root "application#goodbye"
+end
